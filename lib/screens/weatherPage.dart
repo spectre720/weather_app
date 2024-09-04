76f2c5cb42af7475c63cbea9db21b5e8a6adc81a
Amani Godrich <amanimgonja112@gmail.com>
2024-09-04 18:30:11 +0300

    commit prime

---
+++ lib/screens/weatherPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class weatherPage extends StatefulWidget {
+  const weatherPage({super.key});
+
+  @override
+  State<weatherPage> createState() => _weatherPageState();
+}
+
+class _weatherPageState extends State<weatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
